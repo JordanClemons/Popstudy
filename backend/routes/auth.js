cd28479a5327885d1bab1a2efcad4485f6a98988
JordanClemons <jaclemon@buffalo.edu>
2021-05-30 14:17:14 -0500

    Finished login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -33,6 +33,29 @@ router.route('/create').post((req, res) =>{
         .catch(err => res.status(400).json(err));
 });
 
+// Login 
+router.route('/login').get((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.find({"username": username}, function(err, result){
+        if(err){
+            res.status(400).json(err)
+        }
+        else{
+            if(result.length !== 1){res.status(400).json({"result": "Account not found"})}
+            else{
+                const verify = bcrypt.compareSync(password, result[0].hashedPassword);
+                if(!verify){res.status(400).json({"result": "Incorrect information"})}
+                else{
+                    token = generateAccessToken(username);
+                    res.status(200).json(token);
+                }
+            }
+        }
+    })
+})
+
 // Test func - Shows how to get info from token
 // router.get('/test', function(req, res) {
 //     var token = req.headers['authorization'];
